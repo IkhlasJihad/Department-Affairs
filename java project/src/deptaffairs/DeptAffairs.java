df2fa91704301730d3fd2f319604cc99e9c0961f
IkhlasJihad <66485355+IkhlasJihad@users.noreply.github.com>
2021-01-14 13:21:38 +0200

    edit main

---
+++ java project/src/deptaffairs/DeptAffairs.java
@@ -0,0 +1,35 @@
+
+package deptaffairs;
+import Models.Utility;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Ikhlas Jihad 
+ */
+public class DeptAffairs extends Application{
+   
+    public static void main(String[] args) {
+       launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/start.fxml"));
+        stage.setScene(new Scene(root,600,385));
+        stage.setResizable(false);
+        stage.setTitle("Department Affairs");        
+        stage.getIcons().add(new Image("icon.png" ));
+        stage.show();        
+    }
+    @Override
+    public void stop(){
+        Utility.DB.exit();
+    }
+    
+    
+}
