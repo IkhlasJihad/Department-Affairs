90bd27b1774f2e39848e1d9a4a40ce5ffd76228c
IkhlasJihad <ikhlasjaydi@gmail.com>
2021-01-14 22:50:36 +0200

    Final

--- java project/src/Deprtment_Affairs/Department_Affairs.java
+++ java project/src/Deprtment_Affairs/Department_Affairs.java
@@ -1,26 +1,15 @@
-
-<<<<<<< HEAD:java project/src/Deprtment_Affairs/Department_Affairs.java
 package Deprtment_Affairs;
-=======
-package deptaffairs;
->>>>>>> 569ec14a16d847c42f739c81b7fcdff470698bdf:java project/src/deptaffairs/DeptAffairs.java
+
 import Models.Utility;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 /**
- *
  * @author Ikhlas Jihad 
  */
-<<<<<<< HEAD:java project/src/Deprtment_Affairs/Department_Affairs.java
 public class Department_Affairs extends Application{
-=======
-public class DeptAffairs extends Application{
-   
->>>>>>> 569ec14a16d847c42f739c81b7fcdff470698bdf:java project/src/deptaffairs/DeptAffairs.java
     public static void main(String[] args) {
        launch(args);
     }
